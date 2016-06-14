575a56ae0d7afb75e8f109a5d1774a780a0b1b93
Courtney Bradford <courtneyb@wearetelegraph.com>
2016-06-14 15:20:53 -0500

    Client revisions
    Changed primary button colors. Updated mono-700 and 800 values.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,3 +5,6 @@ console.log('\'Allo \'Allo!');
 //# sourceMappingURL=main.js.map
 //# sourceMappingURL=main.js.map
 //# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
